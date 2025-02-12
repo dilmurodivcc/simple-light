b0bb8455253fd7404589ea9a7389c47299c8ff63
dilmurodivcc <zufarovamohira32gmail.com>
2025-02-12 12:30:21 +0500

    done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,48 +1,48 @@
 import Card from "./Card";
 
 // Images of cards
-import img1 from "../assets/icon/Icon-1.svg";
-import img2 from "../assets/icon/Icon 2.svg";
-import img3 from "../assets/icon/Icon-3.svg";
-import img4 from "../assets/icon/Icon-4.svg";
-import img5 from "../assets/icon/Icon-5.svg";
-import img6 from "../assets/icon/Icon-6.svg";
+// import img1 from "../assets/icon/Icon-1.svg";
+// import img2 from "../assets/icon/Icon 2.svg";
+// import img3 from "../assets/icon/Icon-3.svg";
+// import img4 from "../assets/icon/Icon-4.svg";
+// import img5 from "../assets/icon/Icon-5.svg";
+// import img6 from "../assets/icon/Icon-6.svg";
 
 function Cards() {
   let data = [
     {
       id: 1,
-      img: img1,
+      // img: img1,
       title: "Initial Contact",
       p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
       id: 2,
-      img: img6,
+      // img: img6,
       title: "Discovery Session",
       p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
       id: 3,
-      img: img4,
+      // img: img4,
       title: "Contracting",
       p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
       id: 4,
-      img: img2,
+      // img: img2,
       title: "Fast Prototyping",
       p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
       id: 5,
-      img: img3,
+      // img: img3,
       title: "Design Phase",
       p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
       id: 6,
-      img: img5,
+      // img: img5,
       title: "Develop & Launch",
       p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
